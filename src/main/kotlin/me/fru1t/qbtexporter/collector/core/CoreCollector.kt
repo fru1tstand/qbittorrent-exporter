92b632e305ca3c78e26301649e04b61b215943c5
Kodlee Yin <kodlee@fru1t.me>
2019-04-09 21:50:38 -0700

    Conform CoreCollector to new Metric standard
    Rewrote CoreCollector to align with storing a persistent Metric object.

--- src/main/kotlin/me/fru1t/qbtexporter/collector/core/CoreCollector.kt
+++ src/main/kotlin/me/fru1t/qbtexporter/collector/core/CoreCollector.kt
@@ -1,80 +1,65 @@
 package me.fru1t.qbtexporter.collector.core
 
+import me.fru1t.qbtexporter.collector.core.CoreCollectorMetricUtils.singleMetricOf
 import me.fru1t.qbtexporter.prometheus.Metric
 import me.fru1t.qbtexporter.prometheus.MetricType
+import me.fru1t.qbtexporter.prometheus.metric.MultiMetric
 import me.fru1t.qbtexporter.prometheus.metric.SingleMetric
 import me.fru1t.qbtexporter.qbt.response.Maindata
+
+private object CoreCollectorMetricUtils {
+  /**
+   * Creates a [SingleMetric] taking the name from [coreCollector] and defaulting its value to `0`.
+   */
+  internal fun singleMetricOf(coreCollector: CoreCollector, help: String, metricType: MetricType) =
+    SingleMetric(0, coreCollector.name.toLowerCase(), help, metricType)
+
+  /**
+   * Creates a [MultiMetric] taking the name from [coreCollector] and defaulting the metrics to an
+   * empty map.
+   */
+  internal fun multiMetricOf(coreCollector: CoreCollector, help: String, metricType: MetricType) =
+    MultiMetric(mapOf(), coreCollector.name.toLowerCase(), help, metricType)
+}
 
 /** A collector that directly translate the `maindata` qBittorrent api call to metrics. */
 enum class CoreCollector(
   val category: CoreCollectorCategory,
-  val type: MetricType,
-  val help: String,
-  val collect: (Maindata) -> Metric
+  private val update: (Maindata, Metric) -> Unit,
+  private val metric: Metric
 ) {
   ALL_TIME_DOWNLOAD_BYTES(
     CoreCollectorCategory.SERVER,
-    MetricType.COUNTER,
-    "The total number of downloaded bytes from all torrents (including deleted ones) " +
-        "across all sessions.",
-    { maindata ->
-      singleMetricOf(
-        coreCollector = ALL_TIME_DOWNLOAD_BYTES,
-        value = maindata.serverState?.allTimeDownloadedBytes
-      )
-    }
+    { maindata, metric ->
+      (metric as SingleMetric).value = maindata.serverState?.allTimeDownloadedBytes
+    },
+    singleMetricOf(
+      ALL_TIME_DOWNLOAD_BYTES,
+      "The total number of downloaded bytes from all torrents (including deleted ones) across " +
+          "all sessions.",
+      MetricType.COUNTER
+    )
   ),
 
   ALL_TIME_UPLOAD_BYTES(
     CoreCollectorCategory.SERVER,
-    MetricType.COUNTER,
-    "The total number of uploaded bytes from all torrents (including deleted ones) " +
-        "across all sessions.",
-    { maindata ->
-      singleMetricOf(
-        coreCollector = ALL_TIME_UPLOAD_BYTES,
-        value = maindata.serverState?.allTimeUploadedBytes
-      )
-    }
-  )
+    { maindata, metric ->
+      (metric as SingleMetric).value = maindata.serverState?.allTimeUploadedBytes
+    },
+    singleMetricOf(
+      ALL_TIME_DOWNLOAD_BYTES,
+      "The total number of uploaded bytes from all torrents (including deleted ones) across all " +
+          "sessions.",
+      MetricType.COUNTER
+    )
+  );
 
-  ;
+  /** Returns the help string describing what this metric represents. */
+  fun getHelp(): String = metric.help
 
-  private companion object {
-    /** [singleMetricOf] Long values. */
-    private fun singleMetricOf(coreCollector: CoreCollector, value: Long?) =
-      singleMetricOf(
-        coreCollector = coreCollector,
-        value = value?.toDouble()
-      )
-
-    /** [singleMetricOf] Int values. */
-    private fun singleMetricOf(coreCollector: CoreCollector, value: Int?) =
-      singleMetricOf(
-        coreCollector = coreCollector,
-        value = value?.toDouble()
-      )
-
-    /** [singleMetricOf] Boolean values coercing true to `1.0` and false to `0.0`. */
-    private fun singleMetricOf(coreCollector: CoreCollector, value: Boolean?) =
-      singleMetricOf(
-        coreCollector,
-        if (value == true) 1.0 else 0.0
-      )
-
-    /**
-     * Creates a [SingleMetric] of [coreCollector] and its calculated [value]. Automatically coerces
-     * a `null` [value] to `0.0`.
-     */
-    private fun singleMetricOf(
-      coreCollector: CoreCollector,
-      value: Double?
-    ): SingleMetric =
-      SingleMetric(
-        value = value ?: 0.0,
-        name = coreCollector.name.toLowerCase(),
-        help = coreCollector.help,
-        type = coreCollector.type
-      )
+  /** Returns the [Metric] produced by this collector, given [maindata]. */
+  fun collect(maindata: Maindata): Metric {
+    update(maindata, metric)
+    return metric
   }
 }
