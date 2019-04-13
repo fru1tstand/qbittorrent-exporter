72864783b7c5b1e45e95e34e632c762eeb9b59c9
Kodlee Yin <kodlee@google.com>
2019-04-12 20:12:17 -0700

    Add all remaining ServerState telemetry.
    Added the remaining serverstate telemetry for this exporter

--- src/main/kotlin/me/fru1t/qbtexporter/collector/maindata/ServerStateCollector.kt
+++ src/main/kotlin/me/fru1t/qbtexporter/collector/maindata/ServerStateCollector.kt
@@ -19,12 +19,107 @@ enum class ServerStateCollector(
     MetricType.COUNTER,
     { serverState -> serverState?.allTimeDownloadedBytes }
   ),
-
   ALL_TIME_UPLOAD_BYTES(
     "The total number of uploaded bytes from all torrents (including deleted ones) across all " +
         "sessions.",
     MetricType.COUNTER,
     { serverState -> serverState?.allTimeUploadedBytes }
+  ),
+  ALL_TIME_SHARE_RATIO(
+    "The share ratio calculated by taking the all time upload bytes divided by the all time " +
+        "download bytes and rounding to two decimal places.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.allTimeShareRatio?.toDouble() }
+  ),
+  DISK_AVERAGE_CACHE_QUEUE_TIME_MS(
+    "The average time an I/O disk request stays in the queue in milliseconds.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.diskAverageCacheQueueTimeMs }
+  ),
+  DISK_CACHE_SIZE_BYTES(
+    "The number of used bytes (not the maximum) for the disk cache.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.diskCacheSizeBytes }
+  ),
+  DISK_FREE_SPACE_BYTES(
+    "The number of bytes available in the default qBittorrent save path.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.diskFreeSpaceBytes }
+  ),
+  DISK_QUEUE_JOB_COUNT(
+    "The number of disk jobs queued up on the disk cache.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.diskQueueJobCount }
+  ),
+  DISK_QUEUE_SIZE_BYTES(
+    "The number of bytes pending to be flushed to/from disk to/from the disk cache.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.diskQueueSizeBytes }
+  ),
+  DISK_READ_CACHE_HIT_PERCENT(
+    "The percent (rounded to two decimal places) of disk read requests that were handled by the " +
+        "read cache.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.diskReadCacheHitPercent?.toDouble() }
+  ),
+  DISK_READ_CACHE_OVERLOAD_PERCENT(
+    "The percent of connected peers that are issuing read requests (from disk or cache). " +
+        "Literally, the number of peers utilizing disk read I/O divided by the total number " +
+        "of peers connected.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.diskReadCacheOverloadPercent?.toDouble() }
+  ),
+  DISK_WRITE_CACHE_OVERLOAD_PERCENT(
+    "The percent of connected peers that are issuing write requests (from disk or cache). " +
+        "Literally, the number of peers utilizing disk write I/O divided by the total number " +
+        "of peers connected.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.diskWriteCacheOverloadPercent?.toDouble() }
+  ),
+  NETWORK_DHT_NODE_COUNT(
+    "The number of DHT nodes connected.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.networkDhtNodeCount }
+  ),
+  CURRENT_SESSION_DOWNLOADED_BYTES(
+    "The number of bytes downloaded this session.",
+    MetricType.COUNTER,
+    { serverState -> serverState?.currentSessionDownloadedBytes }
+  ),
+  CURRENT_SESSION_UPLOADED_BYTES(
+    "The number of bytes uploaded this session.",
+    MetricType.COUNTER,
+    { serverState -> serverState?.currentSessionUploadedBytes }
+  ),
+  CURRENT_SESSION_WASTED_BYTES(
+    "The number of bytes wasted this session.",
+    MetricType.COUNTER,
+    { serverState -> serverState?.currentSessionBytesWasted }
+  ),
+  NETWORK_PEER_CONNECTION_COUNT(
+    "The number of peers connected to.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.networkPeerConnectionCount }
+  ),
+  NETWORK_DOWNLOAD_SPEED_BYTES_PER_SECOND(
+    "The instantaneous download rate in bytes per second.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.networkDownloadSpeedBytesPerSecond }
+  ),
+  NETWORK_DOWNLOAD_SPEED_CAP_BYTES_SER_SECOND(
+    "The user set maximum download rate in bytes per second.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.networkDownloadSpeedCapBytesPerSecond }
+  ),
+  NETWORK_UPLOAD_SPEED_BYTES_PER_SECOND(
+    "The instantaneous upload rate in bytes per second.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.networkUploadSpeedBytesPerSecond }
+  ),
+  NETWORK_UPLOAD_SPEED_CAP_BYTES_PER_SECOND(
+    "The user set maximum upload rate allowed in bytes per second.",
+    MetricType.GAUGE,
+    { serverState -> serverState?.networkUploadSpeedCapBytesPerSecond }
   );
 
   private companion object {
--- src/test/kotlin/me/fru1t/qbtexporter/collector/maindata/ServerStateCollectorTest.kt
+++ src/test/kotlin/me/fru1t/qbtexporter/collector/maindata/ServerStateCollectorTest.kt
@@ -9,13 +9,31 @@ internal class ServerStateCollectorTest {
   private companion object {
     private val TEST_SERVER_STATE = ServerState(
       allTimeDownloadedBytes = 1,
-      allTimeUploadedBytes = 2
+      allTimeUploadedBytes = 2,
+      allTimeShareRatio = "3.33",
+      diskAverageCacheQueueTimeMs = 4,
+      diskCacheSizeBytes = 5,
+      diskFreeSpaceBytes = 6,
+      diskQueueJobCount = 7,
+      diskQueueSizeBytes = 8,
+      diskReadCacheHitPercent = "9.99",
+      diskReadCacheOverloadPercent = "10.10",
+      diskWriteCacheOverloadPercent = "11.11",
+      networkDhtNodeCount = 12,
+      currentSessionDownloadedBytes = 13,
+      currentSessionUploadedBytes = 14,
+      currentSessionBytesWasted = 15,
+      networkPeerConnectionCount = 16,
+      networkDownloadSpeedBytesPerSecond = 17,
+      networkDownloadSpeedCapBytesPerSecond = 18,
+      networkUploadSpeedBytesPerSecond = 19,
+      networkUploadSpeedCapBytesPerSecond = 20
     )
     private val TEST_DATA = Maindata(serverState = TEST_SERVER_STATE)
 
     /** Returns the metric name for the [serverStateCollector]. */
     private fun metricNameOf(serverStateCollector: ServerStateCollector): String =
-        "qbt_server_state_${serverStateCollector.name.toLowerCase()}"
+      "qbt_server_state_${serverStateCollector.name.toLowerCase()}"
 
     /**
      * Asserts that when passing [TEST_DATA] into the [serverStateCollector], the [expectedOutput]
@@ -30,14 +48,150 @@ internal class ServerStateCollectorTest {
   }
 
   @Test
+  fun collect_returnsNull_whenNotFoundInMaindata() {
+    val emptyMaindata = Maindata()
+
+    ServerStateCollector.values().forEach { collector ->
+      assertThat(collector.collect(emptyMaindata).toString().lines().last())
+        .isEqualTo("${metricNameOf(collector)} 0")
+    }
+  }
+
+  @Test
   fun collect_allTimeDownloadBytes() {
     assertOutput(
-      ServerStateCollector.ALL_TIME_DOWNLOAD_BYTES, TEST_SERVER_STATE.allTimeDownloadedBytes!!)
+      ServerStateCollector.ALL_TIME_DOWNLOAD_BYTES, TEST_SERVER_STATE.allTimeDownloadedBytes!!
+    )
   }
 
   @Test
   fun collect_allTimeUploadBytes() {
     assertOutput(
-      ServerStateCollector.ALL_TIME_UPLOAD_BYTES, TEST_SERVER_STATE.allTimeUploadedBytes!!)
+      ServerStateCollector.ALL_TIME_UPLOAD_BYTES, TEST_SERVER_STATE.allTimeUploadedBytes!!
+    )
+  }
+
+  @Test
+  fun collect_diskAverageCacheQueueTimeMs() {
+    assertOutput(
+      ServerStateCollector.DISK_AVERAGE_CACHE_QUEUE_TIME_MS,
+      TEST_SERVER_STATE.diskAverageCacheQueueTimeMs!!
+    )
+  }
+
+  @Test
+  fun collect_diskCacheSizeBytes() {
+    assertOutput(ServerStateCollector.DISK_CACHE_SIZE_BYTES, TEST_SERVER_STATE.diskCacheSizeBytes!!)
+  }
+
+  @Test
+  fun collect_diskFreeSpaceBytes() {
+    assertOutput(ServerStateCollector.DISK_FREE_SPACE_BYTES, TEST_SERVER_STATE.diskFreeSpaceBytes!!)
+  }
+
+  @Test
+  fun collect_diskQueueJobCount() {
+    assertOutput(ServerStateCollector.DISK_QUEUE_JOB_COUNT, TEST_SERVER_STATE.diskQueueJobCount!!)
+  }
+
+  @Test
+  fun collect_diskQueueSizeBytes() {
+    assertOutput(ServerStateCollector.DISK_QUEUE_SIZE_BYTES, TEST_SERVER_STATE.diskQueueSizeBytes!!)
+  }
+
+  @Test
+  fun collect_diskReadCacheHitPercent() {
+    assertOutput(
+      ServerStateCollector.DISK_READ_CACHE_HIT_PERCENT,
+      TEST_SERVER_STATE.diskReadCacheHitPercent!!.toDouble()
+    )
+  }
+
+  @Test
+  fun collect_diskReadCacheOverloadPercent() {
+    assertOutput(
+      ServerStateCollector.DISK_READ_CACHE_OVERLOAD_PERCENT,
+      TEST_SERVER_STATE.diskReadCacheOverloadPercent!!.toDouble()
+    )
+  }
+
+  @Test
+  fun collect_diskWriteCacheOverloadPercent() {
+    assertOutput(
+      ServerStateCollector.DISK_WRITE_CACHE_OVERLOAD_PERCENT,
+      TEST_SERVER_STATE.diskWriteCacheOverloadPercent!!.toDouble()
+    )
+  }
+
+  @Test
+  fun collect_networkDhtNodeCount() {
+    assertOutput(
+      ServerStateCollector.NETWORK_DHT_NODE_COUNT,
+      TEST_SERVER_STATE.networkDhtNodeCount!!
+    )
+  }
+
+  @Test
+  fun collect_currentSessionDownloadedBytes() {
+    assertOutput(
+      ServerStateCollector.CURRENT_SESSION_DOWNLOADED_BYTES,
+      TEST_SERVER_STATE.currentSessionDownloadedBytes!!
+    )
+  }
+
+  @Test
+  fun collect_currentSessionUploadBytes() {
+    assertOutput(
+      ServerStateCollector.CURRENT_SESSION_UPLOADED_BYTES,
+      TEST_SERVER_STATE.currentSessionUploadedBytes!!
+    )
+  }
+
+  @Test
+  fun collect_currentSessionWastedBytes() {
+    assertOutput(
+      ServerStateCollector.CURRENT_SESSION_WASTED_BYTES,
+      TEST_SERVER_STATE.currentSessionBytesWasted!!
+    )
+  }
+
+  @Test
+  fun collect_networkPeerConnectionCount() {
+    assertOutput(
+      ServerStateCollector.NETWORK_PEER_CONNECTION_COUNT,
+      TEST_SERVER_STATE.networkPeerConnectionCount!!
+    )
+  }
+
+  @Test
+  fun collect_networkDownloadSpeedBytesPerSecond() {
+    assertOutput(
+      ServerStateCollector.NETWORK_DOWNLOAD_SPEED_BYTES_PER_SECOND,
+      TEST_SERVER_STATE.networkDownloadSpeedBytesPerSecond!!
+    )
+  }
+
+  @Test
+  fun collect_networkDownloadSpeedCapBytesPerSecond() {
+    assertOutput(
+      ServerStateCollector.NETWORK_DOWNLOAD_SPEED_CAP_BYTES_SER_SECOND,
+      TEST_SERVER_STATE.networkDownloadSpeedCapBytesPerSecond!!
+    )
+  }
+
+  @Test
+  fun collect_networkUploadSpeedBytesPerSecond() {
+    assertOutput(
+      ServerStateCollector.NETWORK_UPLOAD_SPEED_BYTES_PER_SECOND,
+      TEST_SERVER_STATE.networkUploadSpeedBytesPerSecond!!
+    )
+  }
+
+  @Test
+  fun collect_networkUploadSpeedCapBytesPerSecond() {
+    assertOutput(
+      ServerStateCollector.NETWORK_UPLOAD_SPEED_CAP_BYTES_PER_SECOND,
+      TEST_SERVER_STATE.networkUploadSpeedCapBytesPerSecond!!
+    )
   }
 }
